2de2e7a55ddc8093fb2c009f25ac3a99c6ff79ff
Sophiechuu <chutk.9@gmail.com>
2025-10-16 12:35:02 +0400

    Update game dev intro.cs

---
+++ game dev codes/game dev intro.cs
@@ -0,0 +1,16 @@
+//for example this is the most basic code for variables
+//lvl: beginners
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+        {
+      int myNum = 15;
+      myNum = 20;
+      Console.WriteLine(myNum);
+    }
+  }
+}
